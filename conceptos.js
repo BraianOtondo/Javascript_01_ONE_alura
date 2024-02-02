f9778d9210a788539498536e0baf3eae4b633aa9
BraianOtondo <braian_otondo99@outlook.com>
2024-02-02 11:15:55 -0300

    concepto de prompt agregado

--- conceptos.js
+++ conceptos.js
@@ -1,8 +1,19 @@
 /* 
 alert();
-para proporcionar retroalimentación inmediata o
+se utiliza para proporcionar retroalimentación inmediata o
 informar sobre ciertos eventos en una página web.
 Las alertas detienen el seguimiento web hasta que se de aceptar o cancelar.
 es una función sincronica, quiere decir que primero se ejecuta esta y luego va lo demás
 */
 alert("Hola Mundo");
+/*
+prompt();
+Se utiliza para obtener una entrada del usuario,
+ y funciona de manera similar a un campo de entrada (<input>)
+ en un formulario HTML. Hay algunas diferencias clave.
+ *Crea un cuadro de diálogo modal que detiene la ejecución del script hasta
+ que el usuario interactúa con él, ingresando datos y haciendo clic en "Aceptar" o "Cancelar".
+ *Utiliza en un contexto más interactivo, donde el usuario puede ingresar datos en
+  la página web sin interrumpir la ejecución del código de manera síncrona. 
+*/
+prompt("Indique un número por favor:");
