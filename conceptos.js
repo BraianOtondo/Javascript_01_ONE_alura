09c3713383aa86d2ae102b3b482099459de2513f
BraianOtondo <braian_otondo99@outlook.com>
2024-02-02 10:37:18 -0300

    modificado el concepto de alert mas detallado

--- conceptos.js
+++ conceptos.js
@@ -1,5 +1,8 @@
 /* 
+alert();
 para proporcionar retroalimentación inmediata o
-informar sobre ciertos eventos en una página web
+informar sobre ciertos eventos en una página web.
+Las alertas detienen el seguimiento web hasta que se de aceptar o cancelar.
+es una función sincronica, quiere decir que primero se ejecuta esta y luego va lo demás
 */
 alert("Hola Mundo");
