e206235d5840bb2fd2e33fc09dd4804f4b0c5a4d
BraianOtondo <braian_otondo99@outlook.com>
2024-02-02 09:38:51 -0300

    cambio de nombre de archivo para darselo al principal

--- app.js
+++
@@ -1,3 +0,0 @@
-// para proporcionar retroalimentación
-//inmediata o informar sobre ciertos eventos en una página web
-alert("Hola Mundo");
---
+++ conceptos.js
@@ -0,0 +1,5 @@
+/* 
+para proporcionar retroalimentación inmediata o
+informar sobre ciertos eventos en una página web
+*/
+alert("Hola Mundo");
