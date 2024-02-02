edc433b626c256ed66219868e2a5ca2ff85d2108
BraianOtondo <braian_otondo99@outlook.com>
2024-02-02 12:17:57 -0300

    Juego de adivinar el número con if y else agregado

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 let numeroSecreto=4;
 let numeroUsuario=prompt("Me indicas un número por favor?");
+if(numeroSecreto==numeroUsuario){
+    alert(`Has acertado!!!  el número secreto es ${numeroSecreto}`)
+}else{
+    alert(`Es incorrecto!!!  el número secreto era ${numeroSecreto}`)
+}
