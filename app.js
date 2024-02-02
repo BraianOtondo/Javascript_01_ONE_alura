44725e3c1864a76d8a69a8b8db7b1751cab7b0c1
BraianOtondo <braian_otondo99@outlook.com>
2024-02-02 16:44:32 -0300

    bucle while agregado

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 let numeroSecreto=4;
-let numeroUsuario=prompt("Me indicas un número por favor?");
-if(numeroSecreto==numeroUsuario){
-    alert(`Has acertado!!!  el número secreto es ${numeroSecreto}`)
-}else{
-    alert(`Es incorrecto!!!  el número secreto era ${numeroSecreto}`)
+let numeroUsuario=-1;
+while(numeroSecreto!=numeroUsuario){
+    numeroUsuario=prompt("Indicame un número del 1 al 10");
+    if(numeroSecreto==numeroUsuario ){
+        alert(`Has acertado!!!  el número secreto es ${numeroSecreto} es igual`)//contatenacion
+    }else if(numeroUsuario<numeroSecreto) {
+
+        alert(`Es incorrecto!!!  el número secreto era ${numeroSecreto} es mayor que ${numeroUsuario}`);
+    }else{
+        alert(`Es incorrecto!!!  el número secreto era ${numeroSecreto} es menor que ${numeroUsuario}`);
+
+    }
+
+
 }
