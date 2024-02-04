829eff0819a8af7baac2818f75cff8dcee38bd5e
BraianOtondo <braian_otondo99@outlook.com>
2024-02-04 18:39:17 -0300

    contador agregado y palabra vez arreglado

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 let numeroSecreto=4;
 let numeroUsuario=-1;
+let contador=0;
+let cantidadVeces="vez";
 while(numeroSecreto!=numeroUsuario){
     numeroUsuario=prompt("Indicame un número del 1 al 10");
+    contador++;
     if(numeroSecreto==numeroUsuario ){
-        alert(`Has acertado!!!  el número secreto es ${numeroSecreto} es igual`)//contatenacion
+        alert(`Has acertado!!!  el número secreto es ${numeroSecreto} es igual y lo hiciste ${contador} ${cantidadVeces} `)//contatenacion
     }else if(numeroUsuario<numeroSecreto) {
 
         alert(`Es incorrecto!!!  el número secreto era ${numeroSecreto} es mayor que ${numeroUsuario}`);
@@ -11,6 +14,5 @@ while(numeroSecreto!=numeroUsuario){
         alert(`Es incorrecto!!!  el número secreto era ${numeroSecreto} es menor que ${numeroUsuario}`);
 
     }
-
-
+    cantidadVeces="veces";
 }
