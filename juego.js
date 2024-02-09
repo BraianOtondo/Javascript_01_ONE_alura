e539eedeb8d576288d1a66b53e617a3b832f59e0
BraianOtondo <braian_otondo99@outlook.com>
2024-02-09 20:14:15 -0300

    funciones en javascript agregado

--- juego.js
+++ juego.js
@@ -1,6 +1,11 @@
-let titulo=document.querySelector('h1');//busca el primer h1, si tiene contenido tiene prioridad el html
-titulo.innerHTML=("Juego de adivinar el número");
 //function()
 function intentoUsuario(){
     alert("Haz hecho click.");
 }
+// a diferencia de otros lenguajes, en js las variables no tienen tipo de dato
+function asignarTextoAElemento(elemento,texto){
+    let seleccion=document.querySelector(elemento);
+    seleccion.innerHTML=texto;
+}
+asignarTextoAElemento('h1',"Juego de adivinar el número");
+asignarTextoAElemento("p","Indique un número del 1 al 100");
