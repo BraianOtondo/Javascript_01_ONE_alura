90c34989951ce82b375397cc07aa60d8261486c0
BraianOtondo <braian_otondo99@outlook.com>
2024-02-09 16:59:29 -0300

    Se creo archivo juego.js y agregó al html

--- app.js
+++ app.js
@@ -21,6 +21,5 @@ while(numeroSecreto!=numeroUsuario){
         alert("Superaste el numero de intentos!!");
         break;
     }
-    let titulo=document.querySelector('h1');//busca el primer h1, si tiene contenido tiene prioridad el html
-    titulo.innerHTML=("Juego de adivinar el número");
+
 }
---
+++ juego.js
@@ -0,0 +1,6 @@
+let titulo=document.querySelector('h1');//busca el primer h1, si tiene contenido tiene prioridad el html
+titulo.innerHTML=("Juego de adivinar el número");
+//function()
+function intentoUsuario(){
+    alert("Haz hecho click.");
+}
