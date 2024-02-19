b8f8f1205293d8385a1329fbe38209e5fb43f54f
BraianOtondo <braian_otondo99@outlook.com>
2024-02-19 09:21:34 -0300

    Funcion limpiar input de html con querySelector creado

--- juego.js
+++ juego.js
@@ -1,5 +1,5 @@
 let numeroSecreto=generarNumeroSecreto();
-
+let intentos=0;
 //function()
 function generarNumeroSecreto(){
     return Math.floor(Math.random()*10)+1;
@@ -7,12 +7,15 @@ function generarNumeroSecreto(){
 function verificarIntento(){
     //document es para acceder a objetos del tipo html
     //value accede al valor del elemento encontrado por id
+    intentos++;
     let numeroUsuario=parseInt(document.getElementById('valorUsuario').value);
     //console(numeroSecreto);
     if(numeroUsuario===numeroSecreto){
-        alert("Acertaste el numero");
+        alert(`Acertaste el numero. Con un intento de ${intentos} ${(intentos==1)?"vez":"veces" }`);
+        asignarTextoAElemento("p","GANASTEE!!");
     }else{
-        alert(`No es el numero secreto, era : ${numeroSecreto}`);
+        limpiarCaja();
+        //alert(`No es el numero secreto, era : ${numeroSecreto}`);
     }
 }
 
@@ -21,9 +24,13 @@ function asignarTextoAElemento(elemento,texto){
     let seleccion=document.querySelector(elemento);
     seleccion.innerHTML=texto;
 }
-
+function limpiarCaja(){
+    //document.getElementById es igual a document.querySelector("#id")
+   let valorCaja= document.querySelector("#valorUsuario"); //id de nuestro input 
+    valorCaja.value= '';
+    // una forma de simplificarlo es:
+    //document.querySelector("#valorUsuario").value=' ';
+}
 asignarTextoAElemento('h1',"Juego de adivinar el número");
 asignarTextoAElemento("p","Indique un número del 1 al 10");
 console.log(numeroSecreto);
-//let numeroSecreto=generarNumeroSecreto();
-//verificarIntento(numeroSecreto);
